f7032d51ac3f22bbddcfac9b5c1a6b5443bcce4c
Pirsanth <pirsanth@hotmail>
2022-03-23 07:18:24 +0800

    taking headers out

--- App.js
+++ App.js
@@ -14,12 +14,15 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  TouchableOpacity,
   useColorScheme,
   View,
 } from 'react-native';
+import FontIcon from 'react-native-vector-icons/FontAwesome';
 
 
 const Header = ({showBackButton, title}) => {
+  const COLOR = 'white';
 
   return (
     <View style={{
@@ -27,9 +30,31 @@ const Header = ({showBackButton, title}) => {
       backgroundColor:'powderblue',
       justifyContent:'center',
       alignItems:'center',
-      height:100
+      height:100,
+      flexDirection:'row',
     }}>
-      <Text style={{fontWeight:'bold', color:'white', fontSize:24, textAlign:'center'}}>{title}</Text>
+      {
+        showBackButton && (
+          <TouchableOpacity style={{
+            position:'absolute',
+            width:'10%',
+            left:0,
+            backgroundColor:'red',
+            top:0,
+            bottom:0,
+            justifyContent:'center',
+            alignItems:'center'
+          }}>
+          <FontIcon name="arrow-left" size={22} color={COLOR}/>
+        </TouchableOpacity>
+        )
+      }
+
+      <View style={{width:'80%', justifyContent:'center', alignItems:'center', backgroundColor:'pink'}}>
+        <Text 
+        // ellipsizeMode="tail"
+        style={{fontWeight:'bold', color:COLOR, fontSize:24, textAlign:'center'}}>{title}</Text>
+      </View>
     </View>
   );
 };
@@ -42,7 +67,11 @@ const App = () => {
       flex:1,
       backgroundColor:'orange'
     }}>
-      <Header title="All Posts"/>
+      <Header 
+        title="All Posts"
+        
+        // title="All Posts All Posts All Posts All Posts All Posts All Posts All Posts All Posts All Posts All Posts"
+      />
     </SafeAreaView>
   );
 };
